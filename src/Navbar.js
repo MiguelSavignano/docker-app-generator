05f56c87ce7cad9552d4002746ad9bcd9924ed02
Miguel Savignano <migue.masx@gmail.com>
2020-06-05 22:05:10 +0200

    refactor navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,8 +13,43 @@ const modeLinks = [
   },
 ];
 
-export const Navbar = () => {
+const ModeLink = ({ name, label, ...rest }) => {
   const [, dispatch] = useStateValue();
+
+  return (
+    <a
+      {...rest}
+      className="navbar-item"
+      onClick={() => {
+        const form = initializeFormData(AppModes[name]);
+        dispatch({ form, appMode: AppModes[name] });
+      }}
+    >
+      {label}
+    </a>
+  );
+};
+
+const Logo = () => (
+  <img
+    src="https://1067ec1jtn84131jsj2jmuv3-wpengine.netdna-ssl.com/wp-content/uploads/2017/11/icons-docker-lrg.png"
+    width={50}
+    height={50}
+  />
+);
+
+const GithubRibbon = () => (
+  <a
+    class="github-fork-ribbon"
+    href="https://github.com/MiguelSavignano/docker-app-generator"
+    data-ribbon="Fork me on GitHub"
+    title="Fork me on GitHub"
+  >
+    Fork me on GitHub
+  </a>
+);
+
+export const Navbar = () => {
   return (
     <nav
       className="navbar is-fixed-top is-link"
@@ -23,11 +58,7 @@ export const Navbar = () => {
     >
       <div className="navbar-brand">
         <a className="navbar-item" href="https://bulma.io">
-          <img
-            src="https://1067ec1jtn84131jsj2jmuv3-wpengine.netdna-ssl.com/wp-content/uploads/2017/11/icons-docker-lrg.png"
-            width={50}
-            height={50}
-          />
+          <Logo />
         </a>
         <a
           role="button"
@@ -44,27 +75,12 @@ export const Navbar = () => {
       <div id="navbarBasicExample" className="navbar-menu">
         <div className="navbar-start">
           {modeLinks.map(({ name, label }) => (
-            <a
-              className="navbar-item"
-              onClick={() => {
-                const form = initializeFormData(AppModes[name]);
-                dispatch({ form, appMode: AppModes[name] });
-              }}
-            >
-              {label}
-            </a>
+            <ModeLink className="navbar-item">{label}</ModeLink>
           ))}
         </div>
         <div className="navbar-end">
           <div className="navbar-item">
-            <a
-              class="github-fork-ribbon"
-              href="https://github.com/MiguelSavignano/docker-app-generator"
-              data-ribbon="Fork me on GitHub"
-              title="Fork me on GitHub"
-            >
-              Fork me on GitHub
-            </a>
+            <GithubRibbon />
           </div>
         </div>
       </div>
