693ab13907b71fccbee281cbe9da8327782a2660
Miguel Savignano <migue.masx@gmail.com>
2020-08-23 21:50:53 +0200

    nodejs

--- src/templates/nodejs/TDockerfile.js
+++ src/templates/nodejs/TDockerfile.js
@@ -8,12 +8,10 @@ RUN echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > /app/.npmrc && \\
   rm -f .npmrc;
 <% } -%>
 <% if (javascrit_package_manager === 'npm' && !private_npm) { -%>
-# Node modules cache layer
 COPY package.json package-lock.json ./
 RUN npm ci
 <% } -%>
 <% if (javascrit_package_manager === 'yarn' && private_npm) { -%>
-# Node modules cache layer
 ARG NPM_TOKEN
 COPY package.json yarn.lock ./
 # keep NPM_TOKEN private
@@ -22,20 +20,46 @@ RUN echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > /app/.npmrc && \\
   rm -f .npmrc;
 <% } -%>
 <% if (javascrit_package_manager === 'yarn' && !private_npm) { -%>
-# Node modules cache layer
 COPY package.json yarn.lock ./
 RUN yarn install --pure-lockfile
 <% } -%>`;
 
 module.exports.nodeModulesCacheLayer = nodeModulesCacheLayer;
 
-module.exports.default = `FROM node:<%= node_version %>-alpine
+const typescriptTemplate = `# BUILD STAGE
+FROM node:<%= node_version %>-alpine as build
+
 WORKDIR /app
 
+${nodeModulesCacheLayer}
+
+# Buid with dev dependencies (typescript)
+COPY . .
+RUN npm run build
+RUN rm -r node_modules
+
+# FINAL STAGE
+FROM node:<%= node_version %>-alpine
 ENV NODE_ENV=production
+
+COPY --from=build /app/dist/ /app/dist/
+# Install only production node_modules
+${nodeModulesCacheLayer}
+
+CMD ['npm', 'start']`;
+
+module.exports.default = `<% if (typescript) { -%>
+  ${typescriptTemplate}
+<% } else { -%>
+FROM node:<%= node_version %>-alpine
+WORKDIR /app
+ENV NODE_ENV=production
+
 ${nodeModulesCacheLayer}
 
 COPY . .
 
 CMD ['npm', 'start']
-FROM nodejs`;
+FROM nodejs
+<% } -%>
+`;
