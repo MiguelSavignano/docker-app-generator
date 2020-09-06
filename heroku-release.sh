07d287ec0908b843c24b3407464e03d7864bff1d
Miguel Savignano <migue.masx@gmail.com>
2020-09-06 21:46:26 +0200

    heroku release

--- heroku-release.sh
+++ heroku-release.sh
@@ -1,7 +1,7 @@
 APP_NAME=$1
 WEB_DOCKER_IMAGE_ID=`docker inspect registry.heroku.com/$APP_NAME/web --format={{.Id}}`
 echo $WEB_DOCKER_IMAGE_ID
-curl --netrc -X PATCH https://api.heroku.com/apps/$APP_NAME/formation \
+curl -X PATCH https://api.heroku.com/apps/$APP_NAME/formation \
 --data @<(cat <<EOF
   { "updates": [
       {
@@ -13,4 +13,5 @@ curl --netrc -X PATCH https://api.heroku.com/apps/$APP_NAME/formation \
 EOF
 ) \
 -H "Content-Type: application/json" \
--H "Accept: application/vnd.heroku+json; version=3.docker-releases"
+-H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+-H "Authorization: Bearer $HEROKU_API_KEY"
