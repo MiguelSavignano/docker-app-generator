59c6a75f07cdf004a93000786d5f8c163e11a000
Miguel Savignano <migue.masx@gmail.com>
2021-02-23 02:38:56 +0100

    generate template json file

---
+++ scripts/build-templatedir.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const globby = require('globby');
+
+(async () => {
+  const paths = await globby(['src/templates/**/!(*.js|*.jsx)']);
+  const result = paths.reduce((memo, path) => {
+    const fileContent = fs.readFileSync(path, 'utf-8')
+    memo[path] = Buffer.from(fileContent).toString('base64')
+
+    return memo
+  }, {})
+
+  fs.writeFileSync('src/templates.json', JSON.stringify(result, null, 2))
+})();
