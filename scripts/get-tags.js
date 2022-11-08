0c0ce569aad9598d032c98d0b89684455ab0e6ca
Miguel Savignano <m.savignano@stuart.com>
2022-11-09 00:40:09 +0100

    refactor get tags

--- scripts/get-tags.js
+++ scripts/get-tags.js
@@ -8,26 +8,26 @@ class DockerHubAPI {
     this.allTags = []
   }
 
-  isAlpine(name) {
-    const [_, type] = name.split('-')
-    if (!type) return false
-    return type.includes("alpine")
-  }
-
-  async getTags(url = `https://hub.docker.com/v2/repositories/library/${this.repositoryName}/tags?page_size=100`) {
+  async getTags({url = `https://hub.docker.com/v2/repositories/library/${this.repositoryName}/tags?page_size=100`, filterFnc} = {}) {
     const { data : { results, next }} = await axios.get(url)
-    this.allTags = [...this.allTags, ...results.filter(it => this.isAlpine(it.name) ).map(it => it.name)]
+    this.allTags = [...this.allTags, ...results.filter(filterFnc).map(it => it.name)]
     if (next && this.allTags.length <= 300) {
-      return this.getTags(next)
+      return this.getTags({url: next, filterFnc})
     } else {
       return this.allTags
     }
   }
 }
 
+function isAlpine({name}) {
+  const [_, type] = name.split('-')
+  if (!type) return false
+  return type.includes("alpine")
+}
+
 async function run () {
-  const tags = await new DockerHubAPI('node').getTags()
-  fs.writeFileSync('src/docker-hub-tags/node-alpine.json', JSON.stringify(tags.sort(), null, 2))
+  const tags = await new DockerHubAPI('node').getTags({filterFnc: isAlpine})
+  fs.writeFileSync('src/select-options/node-alpine.json', JSON.stringify(tags.sort(), null, 2))
 }
 
 run()
