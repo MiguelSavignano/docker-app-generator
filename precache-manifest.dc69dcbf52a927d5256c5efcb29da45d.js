9b3130cc1efdcab85ceb3ae566d524c66136e017
MiguelSavignano <migue.masx@gmail.com>
2023-01-27 22:17:08 +0000

    Deploying to gh-pages from @ MiguelSavignano/docker-app-generator@ce00c92198c0522b6bbbb9f50ce57bc684204ceb 🚀

---
+++ precache-manifest.dc69dcbf52a927d5256c5efcb29da45d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de1e7520223acb4492dabce8443c887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66e76e00e5052b5027b3",
+    "url": "/static/css/2.ea2327b6.chunk.css"
+  },
+  {
+    "revision": "31a3c6738acd838a9775",
+    "url": "/static/css/main.bb96a743.chunk.css"
+  },
+  {
+    "revision": "66e76e00e5052b5027b3",
+    "url": "/static/js/2.0a1ae957.chunk.js"
+  },
+  {
+    "revision": "42df45c022c0fbf6a400f50d90bbd697",
+    "url": "/static/js/2.0a1ae957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a3c6738acd838a9775",
+    "url": "/static/js/main.5f242cd7.chunk.js"
+  },
+  {
+    "revision": "e5d4fc46478d256b5188",
+    "url": "/static/js/runtime-main.f3bce04e.js"
+  },
+  {
+    "revision": "8817cfcaa093eb9e58e1d00de481dc1e",
+    "url": "/static/media/docker-logo.8817cfca.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
